dbeeb61a587e6e02ddeca980b6b3c54fa1727ef2
Josed755 <josedanielhs755@gmail.com>
2026-05-14 20:21:34 +0100

    fix visual en la seleccion de ies

--- frontend/src/components/Registrarse.js
+++ frontend/src/components/Registrarse.js
@@ -323,7 +323,7 @@ function Registrarse() {
       <footer className="App-footer">
         <div className="footer-content">
           <div className="footer-section">
-            <h4>{t.titulo}</h4>
+            <h4>CafES App</h4>
             <p>{idioma === 'es' ? 'Horario de pedidos: L-V antes de las 08:00 o después de las 14:00. Fines de semana disponible todo el día.' : 'Order hours: M-F before 08:00 or after 14:00. Weekends available all day.'}</p>
           </div>
           <div className="footer-section">
