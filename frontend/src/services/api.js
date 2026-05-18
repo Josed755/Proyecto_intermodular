b7f478dbbc76ac2943e164344a04c4915b58001f
Josed755 <josedanielhs755@gmail.com>
2026-05-18 13:22:31 +0100

    cambio de cuenta render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://proyecto-intermodular-pmt1.onrender.com/api';
+const API_URL = 'https://cafesapp-backend.onrender.com/api/';
 
 const api = axios.create({
   baseURL: API_URL,
